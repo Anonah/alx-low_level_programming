82452681c51bb3b1ad784dd3005f5cd97e60f06c
Nahom <nahomseyfu3@gmail.com>
2022-12-12 15:54:23 -0800

    betty helps me

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
+/*Description: function to print a line of statme*/
+/* Return: returns a non negative integer */
+
 int main(void)
 {
 	char msg[] = "\"Programming is like building a multilingual puzzle";
+/*function that print a statment0*/
 	puts(msg);
 	return (0);
 }
