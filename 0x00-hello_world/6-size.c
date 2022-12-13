6e406b761610bb9a8e01be4f0f95e4be0a04a313
Nahom <nahomseyfu3@gmail.com>
2022-12-12 17:27:17 -0800

    error

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * Description: function that display size of datatypes
+ * Return: value
+ */
+int main(void)
+{
+printf("Size of a char: ", sizeof(char));
+printf("Size of a char: ", sizeof(int));
+printf("Size of a char: ", sizeof(long));
+printf("Size of a char: ", sizeof(long long));
+printf("Size of a char: ", sizeof(float));
+return (0);
+}
