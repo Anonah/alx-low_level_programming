c123b75f6bdb3031ff3689620574e66c44f086f4
Nahom <nahomseyfu3@gmail.com>
2022-12-12 17:35:13 -0800

    32 and 64

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-printf("Size of a char: %dbyte(s)", sizeof(char));
-printf("Size of a char: %dbyte(s)", sizeof(int));
-printf("Size of a char: %dbyte(s)", sizeof(long));
-printf("Size of a char: %dbyte(s)", sizeof(long long));
-printf("Size of a char: %dbyte(s)", sizeof(float));
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of a char: %d byte(s)\n", sizeof(int));
+printf("Size of a char: %d byte(s)\n", sizeof(long));
+printf("Size of a char: %d byte(s)\n", sizeof(long long));
+printf("Size of a char: %d byte(s)\n", sizeof(float));
 return (0);
 }
