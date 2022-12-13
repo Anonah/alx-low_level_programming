f2ab04c697cd109b525d30e05506399dad36ac1a
Nahom <nahomseyfu3@gmail.com>
2022-12-12 16:45:33 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * Description: function that use printg
+ * Return: non-negative integer
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
