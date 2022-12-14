6ba0182399ad6fff9bf0dd1ed37c9e99db15ae77
Nahom <nahomseyfu3@gmail.com>
2022-12-14 05:36:29 -0800

    no printf and puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include<stdio.h>
+/**
+*main -entry point
+*Return value: one
+*Description: function to print statment without using printf and puts
+*/
+int main(void)
+{
+system("echo'and that piece of art is useful\" - Dora Korpar, 2015-10-19\n'");
+return (1);
+}
