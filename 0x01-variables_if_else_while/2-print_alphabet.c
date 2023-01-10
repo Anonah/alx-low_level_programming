e1698a13c87d7eacb61e8f0c89519825ebf892fc
Nahom <nahomseyfu3@gmail.com>
2023-01-10 10:42:38 -0800

    alphabet ascending

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+* main - entry point
+* Description : print alphabet in lower case in ascending order
+* Return : zero
+*/
+int main(void)
+{
+char alp;
+for(alp='a';alp<='z';alp++)
+{
+putchar(alp);
+}
+putchar('\n');
+return (0);
+}
