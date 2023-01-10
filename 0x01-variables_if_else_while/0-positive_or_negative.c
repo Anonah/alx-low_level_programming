545c483ee64e3740f005ee9e7f69367f4712e14e
Nahom <nahomseyfu3@gmail.com>
2023-01-10 05:02:19 -0800

    positive and negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/**
+* main - entry point
+* Description: function that use printg
+* Return: non-negative integer
+*/
 /* more headers goes there */
 /* betty style doc for function main goes there */
 int main(void)
@@ -9,11 +14,11 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* negative and posetive identifier */
-if(n>0)
+if(n> 0)
 {
 printf("%d is positive \n", n);
 }
-else if(n<0)
+else if(n< 0)
 {
 printf("%d is negative \n", n);
 }
