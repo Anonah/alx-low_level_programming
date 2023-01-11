68054804d46482534c800d1bc98948f24c9800c3
Nahom <nahomseyfu3@gmail.com>
2023-01-10 19:37:46 -0800

    itteration first

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,18 +6,13 @@
 */
 int main(void)
 {
-int num;
-for (num = 0; num <= 9; num++)
-{
-if (num == 9)
+int num = 0;
+while (num <= 9)
 {
 putchar(num + '0');
-}
-else
-{
-putchar(num + '0');
-putchar(',' + ' ');
-}
+num++;
+putchar(',');
+putchar(' ');
 }
 putchar('\n');
 return (0);
