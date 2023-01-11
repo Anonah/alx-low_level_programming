668b14528f938029b43c30b7d9e97a66f1e50eb2
Nahom <nahomseyfu3@gmail.com>
2023-01-10 17:29:11 -0800

    putcharye

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,7 +7,6 @@
 int main(void)
 {
 int num;
-char x[3] = ", ";
 for (num = 0; num <= 9; num++)
 {
 if (num == 9)
@@ -17,7 +16,7 @@ putchar(num + '0');
 else
 {
 putchar(num + '0');
-putchar(x);
+putchar(',' + ' ');
 }
 }
 putchar('\n');
