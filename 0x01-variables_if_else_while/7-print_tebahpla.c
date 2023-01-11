298a4cb336d3e0c20643805f2212508ba1d5b7e9
Nahom <nahomseyfu3@gmail.com>
2023-01-10 16:07:03 -0800

    alphabet in reverse order using putchar()

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+*main - entry point
+*Description : print alphabets in reverse
+*Return: non-negative integer
+*/
+int main(void)
+{
+char alp;
+for (alp = 'z'; alp > 'a'; alp--)
+{
+putchar(alp);
+}
+putchar('\n');
+return (0);
+}
